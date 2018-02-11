95fca344009491ec354220b5b6c2fd31b3e27bad
rama4066 <36346443+rama4066@users.noreply.github.com>
2018-02-11 14:14:30 +0000

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['host'] = 'mysql2';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
